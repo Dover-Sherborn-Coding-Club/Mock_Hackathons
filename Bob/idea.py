1c7e773270727c2bc34969c7815170c3ae1a2c0e
Ben Juo <143736446+rrrr80@users.noreply.github.com>
2023-12-04 15:26:42 -0500

    Update idea.py

--- Bob/idea.py
+++ Bob/idea.py
@@ -1,2 +1,7 @@
-int x = int(input("Hello: \n"))
-
+void main()
+{
+      int x = get_string("What is your name: ");
+      printf("Your name: %s\n", x)
+      x = x * 5 % 10
+      print("Your color is: %G\n", x)
+}
