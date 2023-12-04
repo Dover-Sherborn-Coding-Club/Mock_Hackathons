7a7ce7447e9f531bf18e16d2201c1f27c6fc217c
Ben Juo <143736446+rrrr80@users.noreply.github.com>
2023-12-04 15:30:38 -0500

    Update idea.py

--- Bob/idea.py
+++ Bob/idea.py
@@ -2,6 +2,6 @@ void main()
 {
       int x = get_string("What is your name: ");
       printf("Your name: %s\n", x)
-      x = x * 5 % 10
-      print("Your color is: %G\n", x)
+      long x = double x * 5 % 10
+      printf("Your color is: %G\n", x)
 }
