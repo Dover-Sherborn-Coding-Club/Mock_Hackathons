f393f8ee72e812467584ee99cd1e585ed7c2932e
Ben Juo <143736446+rrrr80@users.noreply.github.com>
2023-12-04 15:39:14 -0500

    Update idea.py

--- Bob/idea.py
+++ Bob/idea.py
@@ -1,9 +1,3 @@
-from math import floor()
+x = input("YA")
 
-void main(number)
-{
-      int x = get_string("What is your name: ");
-      printf("Your name: %s\n", x)
-      long x = double x * 5 % 10
-      printf("Your color is: %G\n", x)
-}
+print(f"{x}")
